966e837d398c878ba33d5770a9de77e49820b175
Min3th <minethweerasinghe@gmail.com>
2026-04-21 14:48:54 +0530

    Updated subscription table in backend.

--- subscription-service/src/main/java/com/track/subscription_service/subscription/entity/Subscription.java
+++ subscription-service/src/main/java/com/track/subscription_service/subscription/entity/Subscription.java
@@ -4,6 +4,9 @@ import com.track.subscription_service.subscription.repository.SubscriptionReposi
 import com.track.subscription_service.user.entity.User;
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 @Entity
 @Table(name = "subscription")
 public class Subscription {
@@ -24,18 +27,39 @@ public class Subscription {
     private String duration;
     private Double cost;
     private String category;
+    private String description;
+
+    @Column(name = "payment_method")
+    private String paymentMethod;
+
+    private String website;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "billing_interval_unit")
+    private String billingIntervalUnit;
+
+    @Column(name = "billing_interval_count")
+    private Integer billingIntervalCount;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
     public Subscription(){}
-    public Subscription(String name, String type, String duration, Double cost,String category) {
+    public Subscription(String name, String type, String duration, Double cost,String category, String description, String paymentMethod,String website, LocalDate startDate, String billingIntervalUnit, Integer billingIntervalCount) {
         this.name = name;
         this.type = type;
         this.duration = duration;
         this.cost = cost;
         this.category = category;
+        this.description = description;
+        this.paymentMethod = paymentMethod;
+        this.website = website;
+        this.startDate = startDate;
+        this.billingIntervalUnit = billingIntervalUnit;
+        this.billingIntervalCount = billingIntervalCount;
     }
 
     public Long getId() {
@@ -86,4 +110,51 @@ public class Subscription {
         this.user = user;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getBillingIntervalUnit() {
+        return billingIntervalUnit;
+    }
+
+    public void setBillingIntervalUnit(String billingIntervalUnit) {
+        this.billingIntervalUnit = billingIntervalUnit;
+    }
+
+    public Integer getBillingIntervalCount() {
+        return billingIntervalCount;
+    }
+
+    public void setBillingIntervalCount(Integer billingIntervalCount) {
+        this.billingIntervalCount = billingIntervalCount;
+    }
 }
