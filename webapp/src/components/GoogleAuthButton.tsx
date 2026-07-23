a182cfd3fd0cd0f57d2d34da5150a1c63b834c87
Min3th <minethweerasinghe@gmail.com>
2026-07-23 09:40:15 +0530

    fix login popup styling.

--- webapp/src/components/GoogleAuthButton.tsx
+++ webapp/src/components/GoogleAuthButton.tsx
@@ -1,15 +1,32 @@
 import { GoogleLogin, type CredentialResponse } from "@react-oauth/google";
+import { Box } from "@mui/material";
 
 type Props = {
   onSuccess: (credentialResponse: CredentialResponse) => void;
+  onError?: () => void;
 };
 
-export default function GoogleAuthButton({ onSuccess }: Props) {
+export default function GoogleAuthButton({ onSuccess, onError }: Props) {
   return (
-    <GoogleLogin
-      onSuccess={onSuccess}
-      onError={() => {
+    <Box
+      sx={{
+        width: "100%",
+        minHeight: 44,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        lineHeight: 0,
       }}
-    />
+    >
+      <GoogleLogin
+        onSuccess={onSuccess}
+        onError={onError}
+        shape="pill"
+        size="large"
+        text="continue_with"
+        logo_alignment="left"
+        width="400"
+      />
+    </Box>
   );
 }
